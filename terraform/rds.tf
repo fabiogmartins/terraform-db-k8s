84560f6bb0a0fe7d2f94ae859ec725fa8625f7df
Fábio Guimarães Martins <fabiogmartins@gmail.com>
2024-03-13 00:01:26 -0300

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,6 +1,6 @@
 resource "aws_db_instance" "postgres" {
   engine                  = "postgres"
-  engine_version          = "12.4"
+  engine_version          = "14.2"
   instance_class          = "db.t3.micro"
   allocated_storage       = 20
   max_allocated_storage   = 100
