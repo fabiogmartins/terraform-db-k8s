b84f19ca82dd086d41d68493d91195ec339e9650
fabiogmartins <fabiogmartins@gmail.com>
2024-03-13 08:06:42 -0300

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -9,7 +9,7 @@ resource "aws_db_subnet_group" "my_db_subnet_group" {
 
 resource "aws_db_instance" "postgres" {
   engine                  = "postgres"
-  engine_version          = "13.3"
+  engine_version          = "13.8"
   instance_class          = "db.t3.micro"
   allocated_storage       = 20
   max_allocated_storage   = 100
