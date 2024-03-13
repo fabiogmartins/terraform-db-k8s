ea6231f65f866592c22cfbb93362cde8097c1f77
Fábio Guimarães Martins <fabiogmartins@gmail.com>
2024-03-12 23:38:48 -0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,9 @@
-provider "aws" {
-  region  = var.aws_region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.72.0"
+      region  = var.aws_region
+    }
+  }
 }
