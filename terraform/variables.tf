c9aae566f24c8b48e2772117edb23ef59b273c90
Fábio Guimarães Martins <fabiogmartins@gmail.com>
2024-03-12 22:59:33 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,13 +6,16 @@ variable "aws_region" {
 
 variable "db_username" {
   description = "Username for the RDS database instance"
+  type        = string
 }
 
 variable "db_password" {
   description = "Password for the RDS database instance"
+  type        = string
   sensitive   = true
 }
 
 variable "database_name" {
   description = "Database name for the RDS database instance"
+  type        = string
 }
