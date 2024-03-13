558c57c169207b803fa6ca307848e90b9305455c
Fábio Guimarães Martins <fabiogmartins@gmail.com>
2024-03-12 23:42:26 -0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,14 @@
 terraform {
+  required_version = ">= 0.14.9" # Garante que você está usando uma versão compatível do Terraform
+
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.40" # Especifique a versão compatível aqui
+      source  = "hashicorp/aws" # O provedor da AWS
+      version = "~> 3.72.0"       # Especifica a versão do provedor da AWS. Ajuste conforme necessário.
     }
   }
+}
+
+provider "aws" {
+  region = var.aws_region
 }
