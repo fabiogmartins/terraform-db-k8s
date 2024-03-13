be564236c1d19de98f3dd54a3c2ac56748651f37
fabiogmartins <fabiogmartins@gmail.com>
2024-03-13 20:43:18 -0300

    Update eks-cluster.tf

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -3,7 +3,7 @@ module "eks" {
   version         = "18.14.0"
   cluster_name    = "eks-cluster"
   cluster_version = "1.22"
-  subnets         = module.vpc.private_subnets_ids
+  subnets         = module.vpc.private_subnets
 
   vpc_id = module.vpc.vpc_id
 
