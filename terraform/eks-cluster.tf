3c409bdeb7c5e64a99252c27ae9207d625c8e51d
fabiogmartins <fabiogmartins@gmail.com>
2024-03-13 21:34:41 -0300

    Update eks-cluster.tf

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -2,10 +2,16 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "17.24.0"
   cluster_name    = "eks-cluster"
-  cluster_version = "1.22"
+  cluster_version = "1.25"
   subnets         = module.vpc.private_subnets
 
   vpc_id = module.vpc.vpc_id
+
+  worker_groups = [
+    {
+      instance_type = "t3.micro"
+      asg_max_size  = 1
+    }
 
   node_groups = {
     ng1 = {
