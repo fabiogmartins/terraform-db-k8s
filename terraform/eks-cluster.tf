ca7c2876f92bbf131ce849e1afc37a0523165f95
fabiogmartins <fabiogmartins@gmail.com>
2024-03-15 20:50:51 -0300

    Update provider.tf e eks-cluster.tf

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -26,3 +26,11 @@ module "eks" {
     }
   }
 }
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -12,3 +12,11 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+provider "kubernetes" {
+  config_path = "~/.kube/config" # Ou o caminho para o seu kubeconfig específico do EKS
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
