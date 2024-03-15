a4f4e23db1b58dc80655bf60cec993e3841d4e76
fabiogmartins <fabiogmartins@gmail.com>
2024-03-15 19:13:34 -0300

    Update eks-cluster.tf

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -11,6 +11,7 @@ module "eks" {
     {
       instance_type = "t3.micro"
       asg_max_size  = 1
+      disk_size     = 1
     }
   ]
 
@@ -21,6 +22,7 @@ module "eks" {
       min_capacity     = 1
 
       instance_types   = ["t3.micro"]
+      disk_size        = 1  
     }
   }
 }
