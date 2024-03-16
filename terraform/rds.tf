9525b7d0fc3f3aa9426b1e5116c73c39e5bf9ce5
Fábio Guimarães Martins <fabiogmartins@gmail.com>
2024-03-16 11:28:06 -0300

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -15,7 +15,6 @@ resource "aws_db_instance" "postgres" {
   username                = var.db_username
   password                = var.db_password
   name                    = var.database_name
-  db_subnet_group_name    = aws_default_vpc.default.name
   vpc_security_group_ids  = [aws_security_group.rds_sg.id]
   skip_final_snapshot     = true
   deletion_protection     = false
@@ -25,8 +24,6 @@ resource "aws_db_instance" "postgres" {
   tags = {
     "Name" = "my-postgres-db"
   }
-
-  depends_on = [aws_default_vpc.default]
 }
 
 resource "aws_security_group" "rds_sg" {
