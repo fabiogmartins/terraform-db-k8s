977ba7322a900d303a7dc298222370d7b9796871
fabiogmartins <fabiogmartins@gmail.com>
2024-03-15 21:18:00 -0300

    update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -17,7 +17,7 @@ resource "aws_db_instance" "postgres" {
   username                = var.db_username
   password                = var.db_password
   name                    = var.database_name
-  db_subnet_group_name = "my-dbsubnet-group"
+  db_subnet_group_name    = aws_db_subnet_group.my_db_subnet_group.name
   vpc_security_group_ids  = [aws_security_group.rds_sg.id]
   skip_final_snapshot     = true
   deletion_protection     = false
@@ -26,6 +26,8 @@ resource "aws_db_instance" "postgres" {
   tags = {
     "Name" = "my-postgres-db"
   }
+
+  depends_on = [aws_db_subnet_group.my_db_subnet_group]
 }
 
 resource "aws_security_group" "rds_sg" {
