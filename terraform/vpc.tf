57402df795a25e86d0d731a940375e45261f7a70
Fábio Guimarães Martins <fabiogmartins@gmail.com>
2024-03-16 10:24:55 -0300

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -4,6 +4,8 @@ module "vpc" {
 
   name = "k8s-vpc"
   cidr = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
