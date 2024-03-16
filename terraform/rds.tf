0d059957d4cb418d30f454c0bc61533a0e24a292
Fábio Guimarães Martins <fabiogmartins@gmail.com>
2024-03-16 09:39:47 -0300

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -39,7 +39,7 @@ resource "aws_security_group" "rds_sg" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = [module.vpc.vpc_cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
